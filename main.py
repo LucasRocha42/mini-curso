1bb9851ab2371ebbd89234dbd9f525d192515e76
lucasrocha42 <lucasfariascr1@gmail.com>
2021-12-24 17:00:54 -0200

    Finalizado

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Importa a base de dados
 import pandas as pd # importando a biblioteca 
+import win32com.client as win32 # Importando 
+
 tabela_vendas = pd.read_excel('Vendas.xlsx') # Integrando excel com python 
 
 # Visualizar a base de dados 
@@ -17,10 +19,36 @@ quantidade = tabela_vendas [['ID Loja','Quantidade']].groupby('ID Loja').sum()
 print(quantidade)
 
 # Ticket médio por produto em cada loja 
-ticket = faturamento/quantidade 
-
+ticket = faturamento(['Valor Final'] / quantidade['Quantidade']).to_frame() # --to.frame-- transforma em tabela 
+ticket = ticket.rename(columns={0: 'Ticket Médio'})
+print(ticket)
 
 # Enviar email com relatório
+outlook = win32.Dispatch('outlook.aplication') # conectando python com outlook
+mail = outlook.CreateItem(0) # Criando um email 
+mail.To = 'To andress' # Para quem vai enviar
+mail.Subject = 'Message subject' # Assunto do email
+mail.HTMLBody =  f''' 
+<p> </p> Prezados,
+ 
+
+ <p> Segue o Relatório de Vendas por cada loja. </p>
+
+ <p> Faturamento: {faturamento.to_html(formatters={'Valor final': 'R${:,.2f}'.format })} </p>
+
+ <p> Quantidade Vendida {quantidade.to_html}: </p>
+
+ <p> Ticket Medio {ticket.to_html(formatters={'Ticket': 'R${:,.2f}'.format })} </p>
+
+ <p> Qualquer dúvida estou à disposição </p>
+
+''' # Corpo do email
+
+print('Email Enviado')
+
+
+
+
 
 
 
