696bd2873c3b425c0c2d9b7f54a6ca9c7225e024
lucasrocha42 <lucasfariascr1@gmail.com>
2021-12-24 12:11:58 -0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mini-curso
+Análise de dados em cima de uma base de dados e enviar um relatório por email
+- Faturamento por loja 
+- Quantidade de produto 
+- ticket de venda
+
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hellow")
+# Importa a base de dados
+import pandas as pd # importando a biblioteca 
+
+# Visualizar a base de dados 
+tabela_vendas = pd.read_excel('Vendas.xlsx') 
+
+# Calcular o faturamento por loja 
+# Quantidade de produtos por loja 
+# Ticket médio por produto em cada loja 
+# Enviar email com relatório
