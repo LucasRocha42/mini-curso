238effcac0d9f1a21abca4c32e25eb11bd756c1e
lucasrocha42 <lucasfariascr1@gmail.com>
2021-12-24 16:25:08 -0200

    Ticket

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 # Importa a base de dados
 import pandas as pd # importando a biblioteca 
+tabela_vendas = pd.read_excel('Vendas.xlsx') # Integrando excel com python 
 
 # Visualizar a base de dados 
-tabela_vendas = pd.read_excel('Vendas.xlsx') 
+pd.set_option('display.max.columns', None ) # Não tem máximo de colunas
+print(tabela_vendas)
 
 # Calcular o faturamento por loja 
+faturamento = tabela_vendas [['ID Loja', 'Valor Final']].groupby('ID Loja').sum() 
+print(faturamento)
+    #Filtrando tabela entre loja e faturamento
+    # -groupby- Agrupando todas as lojas e -sum- soma os faturamento 
+
 # Quantidade de produtos por loja 
+quantidade = tabela_vendas [['ID Loja','Quantidade']].groupby('ID Loja').sum()
+print(quantidade)
+
 # Ticket médio por produto em cada loja 
+ticket = faturamento/quantidade 
+
+
 # Enviar email com relatório
+
+
+
+
